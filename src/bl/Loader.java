f1a461d137289e42072004d92900c5f2e3d610b5
smoroa14 <robertscho1240@gmail.com>
2018-09-17 19:05:45 +0200

    input verzeichnis sucher

--- src/bl/Loader.java
+++ src/bl/Loader.java
@@ -4,17 +4,32 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Loader {
+
+  private static String src;
+
+  public static void loadConfig()
+  {
+    try (BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + File.separator + "config.config"))) {
+      src = br.readLine().split(": ")[1];
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
   public static Icon loadImage(String filename)
   {
+    loadConfig();
     BufferedImage img = null;
 
     try
     {
-      filename = System.getProperty("user.dir")+File.separator+"src"+File.separator+"images"+File.separator+filename;
+      filename = System.getProperty("user.dir")+src+"images"+File.separator+filename;
       System.out.println(filename);
       img = ImageIO.read(new File(filename));
     }
